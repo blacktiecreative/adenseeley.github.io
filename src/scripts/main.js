c935f43747ff4d159dc8b958e576f29d0ba005c0
adenseeley <adenseeley12@gmail.com>
2015-06-25 05:54:24 -0400

    made the nav and ripped out some of the html to make it better because it sucked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,12 @@
 window.$ = require('jquery')
 
 require('velocity')
-console.log("'Allo, 'Allo!");
+
+$(function() {
+    var body = $('body');
+    var menus = $('.menu','.menu-side');
+    $('.menu-toggle').bind('click', function(){
+    	body.toggleClass('menu-open');
+    	return false;
+    }); 
+});
